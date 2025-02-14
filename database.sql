9ba8ede2a5ee01368e6f999c37b9916ff9bad0a3
teagar <cyanofc@gmail.com>
2025-02-14 10:33:41 -0300

    Update: database tables

--- database.sql
+++ database.sql
@@ -3,8 +3,7 @@ use noticeboard;
 
 CREATE TABLE IF NOT EXISTS users(
   id INT AUTO_INCREMENT PRIMARY KEY,
-  `name` VARCHAR(255) NOT NULL,
-  email VARCHAR(255) NOT NULL,
+  `login` VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL
 );
 
