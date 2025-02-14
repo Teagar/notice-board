3817a2793e0f397e3457a1218e1b4cf8c44eb613
teagar <cyanofc@gmail.com>
2025-02-14 11:55:36 -0300

    Add: notice edit page

--- v/notices/edit.php
+++ v/notices/edit.php
@@ -1,48 +1,48 @@
 <?php
   require_once $_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php';
   
-  if(empty($_GET['notices']) || !isset($_SESSION['id'])){
+  if(empty($_GET['notice']) || !isset($_SESSION['id'])){
     header("Location: /");
   }
 
   use App\Controllers\Notice;
-  $Aviso = new Notice();
-  $aviso = $Aviso->selecionar(["id"=>$_GET['aviso']]);
-  if(is_array($aviso) && count($aviso) > 0){
-    $aviso = $aviso[0];
+  $Notice = new Notice();
+  $notice = $Notice->select(["id"=>$_GET['notice']]);
+  if(is_array($notice) && count($notice) > 0){
+    $notice = $notice[0];
   }
 ?>
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="en-us">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Edição de Avisos</title>
+  <title>Notice Edit</title>
 
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
   <div class="container">
-    <form action="/api/avisos/editar.php" class="w-50 mt-5 m-auto" method="post">
-      <input type="hidden" name="aviso" value="<?= $aviso['id'] ?>">
-      <h3 class="text-center">Editar Aviso</h3>
+    <form action="/api/notices/edit.php" class="w-50 mt-5 m-auto" method="post">
+      <input type="hidden" name="notice" value="<?= $notice['id'] ?>">
+      <h3 class="text-center">Edit Notice</h3>
       <div class="mb-3">
-        <label for="titulo" class="form-title">Título</label>
-        <input type="text" class="form-control" name="titulo" id="titulo" value="<?= $aviso['titulo'] ?>" required>
+        <label for="title" class="form-title">Title</label>
+        <input type="text" class="form-control" name="title" id="title" value="<?= $notice['title'] ?>" required>
       </div>
 
       <div class="mb-3">
         <label for="link" class="form-title">Link</label>
-        <input type="text" class="form-control" id="link" name="link" value="<?= $aviso['link'] ?>">
+        <input type="text" class="form-control" id="link" name="link" value="<?= $notice['link'] ?>">
       </div>
 
       <div class="mb-3">
-        <label for="descricao" class="form-title">Descrição</label>
-        <textarea class="form-control" name="descricao" id="descricao" required><?= $aviso['descricao'] ?></textarea>
+        <label for="description" class="form-title">Description</label>
+        <textarea class="form-control" name="description" id="description" required><?= $notice['description'] ?></textarea>
       </div>
 
       <div class="mb-3">
-        <input type="submit" class="btn w-100 btn-primary" value="Editar">
+        <input type="submit" class="btn w-100 btn-primary" value="Edit">
       </div>
     </form>
   </div>
