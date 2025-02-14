b4f8683112907cf209f0c47e8ab11ad220136751
teagar <cyanofc@gmail.com>
2025-02-14 12:25:06 -0300

    Add: index page

---
+++ v/index.php
@@ -0,0 +1,92 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php';
+
+use App\Controllers\User;
+use App\Controllers\Notice;
+
+$User = new User();
+$Notice   = new Notice();
+
+$userExists = (bool)$User->select();
+
+$notices = $Notice->select();
+?>
+<!DOCTYPE html>
+<html lang="en-us">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Notice Board</title>
+
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+  <header class="text-center border-bottom p-1 d-flex justify-content-between align-items-center px-3">
+    <h1 class="fs-4">Notice Board</h1>
+
+    <?php
+      if(!$userExists){
+        $href = "/v/users/register.php";
+        $text = "Sign-up";
+      } else {
+        $href = "/v/users/login.php";
+        $text = "Login";
+      }
+    ?>
+
+    <div>
+      <?php
+        if(!isset($_SESSION['id'])){
+          ?>
+            <a href="<?= $href ?>"><?= $text ?></a>
+          <?php
+        } else {
+          ?>
+            <a href="/v/users/edit.php" style="margin-left: 10px;">Edit Profile</a>
+            <a href="/v/notices/register.php" style="margin-left: 10px;">Register</a>
+            <a href="/api/users/exit.php" style="margin-left: 10px;">Exit</a>
+          <?php
+        }
+      ?>
+    </div>
+  </header>
+
+  <div class="container">
+    <?php
+      if(is_array($notices) && count($notices) > 0){
+        foreach($notices as $notice){
+          ?>
+            <div class="card w-100 mt-2 shadow">
+              <div class="card-body">
+                <h5 class="card-title"><?= htmlspecialchars($notice['title']) ?></h5>
+                <h6 class="card-subtitle mb-2 text-body-secondary"><?= date('d/m/Y H:i', strtotime($notice['creation_date'])) ?></h6>
+                <p class="card-text"><?= htmlspecialchars($notice['description']) ?></p>
+  
+                  <?php
+                    if(!empty($notice['link'])){
+                      ?>
+                        <a href="<?= $notice['link'] ?>" target="_blank" class="card-link">Access</a>
+                        
+                        <?php
+                    }
+
+                    if(isset($_SESSION['id'])){
+                      ?>
+                        <a href="/v/notices/edit.php?notice=<?= $notice['id'] ?>">Edit</a>
+                        <a href="/api/notices/delete.php?notice=<?= $notice['id'] ?>">Delete</a>
+                      <?php
+                    }
+                  ?>
+              </div>
+            </div>
+          <?php
+        }   
+      } else {
+        echo "<p class=\"text-center\">Nothing to show here.</p>";
+      }
+    ?>
+  </div>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
