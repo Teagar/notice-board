b707817f9b05944a526a94e54c72b6e80ff30872
teagar <cyanofc@gmail.com>
2025-02-14 10:54:37 -0300

    Add: Register api

---
+++ api/notices/register.php
@@ -0,0 +1,21 @@
+<?php
+  require_once $_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php';
+
+  if($_SERVER['REQUEST_METHOD'] != 'POST'){
+    header("Location: /v/notices/register.php");
+    exit;
+  }
+
+  if(empty($_POST['title']) || empty($_POST['description'])){
+    header("Location: /v/notices/register.php");
+    exit;
+  }
+
+  use App\Controllers\Notice;
+
+  $Aviso = new Notice();
+
+  $Aviso->create($_POST['title'], $_POST['description'], $_POST['link']);
+
+  header("Location: /");
+  exit;
